70f714373e24db78a560cd7d315a622719e9960b
Вадим <vadim.ppp@mail.ru>
2021-11-17 11:30:33 +0500

    Добавьте файлы проекта.

---
+++ Project39/Source.cpp
@@ -0,0 +1,24 @@
+#include "omp.h"
+#include "stdio.h"
+#include <ctime>
+#include "windows.h"
+#include <iostream>
+#include "tchar.h"
+using namespace std;
+
+
+
+int main()
+{
+	omp_set_num_threads(3);
+	#pragma omp parallel 
+	{
+
+		printf("I am %d thread from %d threads!\n",
+			omp_get_thread_num(),
+			omp_get_num_threads()
+		);
+	}
+
+	getchar();
+}
