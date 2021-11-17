138095009195fc70147c4b18f9fde31c611bee64
Вадим <vadim.ppp@mail.ru>
2021-11-17 11:46:18 +0500

    Test2

--- Project39/Source.cpp
+++ Project39/Source.cpp
@@ -13,7 +13,8 @@ int main()
 	omp_set_num_threads(3);
 	#pragma omp parallel 
 	{
-		PRIVET KY KY KY
+		PRIVET KY KY KY 
+			POKA POKA
 		printf("I am %d thread from %d threads!\n",
 			omp_get_thread_num(),
 			omp_get_num_threads()
